7efbafbe7a4b0be915ae5b1362febc1284194d35
Taapas Sharma <131648154+TaapasSharma@users.noreply.github.com>
2024-04-29 22:19:48 +0530

    Build a Flask app with static HTML pages and navigate between them.

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-from flask import Flask
+from flask import Flask , render_template, request
+import requests 
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    return "<h1>Hello, World!</h1>"
+@app.route('/')
+def homepage():
+    return render_template("index.html")
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0", port=5003)
+@app.route("/weatherapp",methods = ['POST' , "GET"])
+def get_weatherdata():
+    url = "https://api.openweathermap.org/data/2.5/weather"
 
-# https://mango-accountant-dvdsi.pwskills.app:5003
+    param = {
+        'q':request.form.get("city"),
+        'appid':request.form.get('appid'),
+        'units':request.form.get('units')
+        }
+
+    response = requests.get(url,params=param)
+
+    data = response.json()
+    
+    return f"data : {data}"
+
+if __name__ == '__main__':
+    app.run(host= "0.0.0.0" , port = 5001)
+    
+    
